7e7c5498d19f3de921065f13bcb5c376c6b83985
Shelley Zhang <shelleyyz91@gmail.com>
2018-06-13 23:15:33 +1000

    calc and strings hw

---
+++ shelley_zhang/week_01/day_03/strings/js/strings.js
@@ -0,0 +1,58 @@
+//DrEvil
+
+const DrEvil = function(amount) {
+  if (amount === 1000000) {
+      return amount + " dollars" + " (pinky)";
+  } else {
+    return amount + " dollars";
+  }
+};
+
+DrEvil(100);
+
+//mixUp
+
+const mixUp = function(string1, string2) {
+ const new1 = string1.slice(0,2) + string2.slice(2, string2.length);
+ const new2 = string2.slice(0,2) + string1.slice(2, string1.length);
+ console.log(new1, new2);
+}
+
+mixUp("Dogger", "Catter");
+
+//fixStart
+
+const fixStart = function(string) {
+  const splitter = string.split(""); //splitting the array up into letters
+for (var i = 1; i < string.length; i++) {
+  if (splitter[i] === string[0]) {
+    splitter[i] = "*";
+  }
+}
+const combine = splitter.join("");
+console.log(combine);
+return combine;
+};
+
+fixStart("bobby");
+
+//verbing
+
+const verbing = function(string) {
+
+  if (string.length >= 3) {
+    console.log(string + "ing");
+  } else if (string.length >= 3 && string.endsWith("ing")) { //why doesn't this work?
+    console.log(string + "ly");
+  } else {
+    console.log(string);
+  }
+};
+
+verbing("si");
+
+//Not Bad
+
+const notBad = function(string) {
+  
+}

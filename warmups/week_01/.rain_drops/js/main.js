bc233d6d0de93ab56fe9504d0f76128fe28af0b2
John Coote <john_coote@rocketmail.com>
2018-06-14 10:11:48 +1000

    warm 1

---
+++ warmups/week_01/.rain_drops/js/main.js
@@ -0,0 +1,44 @@
+// console.log("connected");
+//
+//
+// Warmup - Raindrops
+// Write a Javascript program that will take a number (eg 28 or 1755 or 9, etc) and output the following:
+//
+// If the number contains 3 as a factor, output 'Pling'.
+// If the number contains 5 as a factor, output 'Plang'.
+// If the number contains 7 as a factor, output 'Plong'.
+// If the number does not contain 3, 5, or 7 as a factor, output the number as a string.
+// Examples
+// 28 has 7 as a factor.
+// In raindrop-speak, this would be a simple "Plong".
+// 1755 has 3 and 5 as factors.
+// In raindrop-speak, this would be a "PlingPlang".
+// 34 has neither 3, 5 nor 7 as a factor.
+// Raindrop-speak doesn't know what to make of that, so it just goes with the straightforward "34".
+
+const plong = function(num) {
+
+  result = ""
+
+  if (num % 3 === 0) {
+    result += "Pling"
+  }
+
+  if (num % 5 === 0) {
+    result += "Plong"
+  }
+
+  if (num % 7 === 0) {
+    result += "Plang"
+  }
+
+  if (result === "") {
+    result = num
+  }
+
+  return result
+}
+
+console.log( plong(34) );
+console.log( plong(15) );
+console.log( plong(210) );

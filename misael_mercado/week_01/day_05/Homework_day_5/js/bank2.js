20ab806cacf5955cef5cd78a2b6e3671642a7f32
Misael M <misaelmdev@gmail.com>
2018-06-19 14:03:46 +1000

    alternative methods from homework on JavaScript

---
+++ misael_mercado/week_01/day_05/Homework_day_5/js/bank2.js
@@ -0,0 +1,31 @@
+
+///alternative method
+
+const bank = {
+  accounts: [
+    {
+ name: "john",
+ balance: 100
+
+},
+{
+  name: "Misael M",
+  balance: 2000;
+}
+],
+
+addAccount: function (name) {
+  let newAccount = {
+    name: newName,
+    balance: 0
+  }
+  bank.aacounts.push(newAccount);
+},
+
+deposit: function (name, value) {
+
+},
+withdrawal: function (name, value) {
+
+}
+};

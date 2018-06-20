81ce6ea47c9e3ae30073f67039c94394cbbba339
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-21 09:35:23 +1000

    Homework Update

--- lucas_padua/week_02/day_03/cat-walk/js/main.js
+++ lucas_padua/week_02/day_03/cat-walk/js/main.js
@@ -7,21 +7,21 @@ img.style.left = '0px';
 let move = +1;
 
 const catWalk = function() {
-  let oldLeft = parseInt(img.style.left);
-  let newLeft = oldLeft + move;
+  const oldLeft = parseInt(img.style.left);
+  const newLeft = oldLeft + move;
   img.style.left = newLeft + 'px';
 
   // if ( newLeft > 1200) {
-    if(newLeft>=window.innerWidth) {
+  if (newLeft >= window.innerWidth) {
     // img.style.left = "0px"
     move = -1;
-    img.style.webkitTransform ="scaleX(-1)";
+    img.style.webkitTransform = "scaleX(-2)";
 
-  }else if  (newLeft === 0){
+  } else if (newLeft === 0) {
 
-     move = +1
-     img.style.webkitTransform ="scaleX(+1)";
+    move = +1
+    img.style.webkitTransform = "scaleX(+3)";
   }
 };
 
-setInterval(catWalk, 1);
+setInterval(catWalk, .1);

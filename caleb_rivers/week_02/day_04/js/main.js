e0189e393fb3982d9d9eb1641142415d7ad8a395
Caleb Rivers <cal1riv@gmail.com>
2018-06-25 08:53:04 +1000

    Bank of GA

---
+++ caleb_rivers/week_02/day_04/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  $('#button1').on('click', function () {
+    $('#box1').slideToggle();
+  })
+
+  $('#button2').on('click', function () {
+    $('#box2').slideToggle();
+  })
+
+  $('#button3').on('click', function () {
+    $('#box3').slideToggle();
+  })
+})

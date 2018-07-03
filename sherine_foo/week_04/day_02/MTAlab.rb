fb6c18541a5631636ce20ef5aff628ada90f6f61
Sherine <sherinemay79@gmail.com>
2018-07-03 22:59:05 +1000

    completed all request

--- sherine_foo/week_04/day_02/MTAlab.rb
+++ sherine_foo/week_04/day_02/MTAlab.rb
@@ -23,12 +23,13 @@ require 'pry'
 # All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
 # Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
 # Hints:
-# Work out how you would do it on paper first! Then start to explain that process in Ruby.
+# Work out how you wtould do it on paper first! Then start to explain that process in Ruby.
 # Get the program to work for a single line before trying to tackle multiple lines.
 # Don't worry about prompting the user for input. Hard code some values to get it working. You can use gets later to make it more interactive.
 # Consider diagramming the lines by sketching out the subway lines and their stops and intersection.
 # The key to the lab is finding the index positions of each stop. (hint: index)
 # Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
+
 
 
 @train_line = {
@@ -37,14 +38,20 @@ require 'pry'
   "6" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
 
-# print "Which line are you departing : "
-# line_start = gets.chomp
-# print "Which stop are you departing : "
-# start = gets.chomp
-# print "Which line are you arriving : "
-# line_stop = gets.chomp
-# print "Which stop are you arriving : "
-# stop = gets.chomp
+
+p "There are 3 subway lines:"
+p 'The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th'
+p 'The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st'
+p 'The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.'
+
+print "Which line are you departing : "
+line_start = gets.chomp
+print "Which stop are you departing : "
+start = gets.chomp
+print "Which line are you arriving : "
+line_stop = gets.chomp
+print "Which stop are you arriving : "
+stop = gets.chomp
 
 def plan_trip (line_start, start, line_stop, stop)
   start_stop = @train_line[line_start].index start
@@ -57,49 +64,32 @@ def plan_trip (line_start, start, line_stop, stop)
     new_trip = @train_line[line_start][start_stop..end_stop]
     total_stop = new_trip.length
 
-    p "You must travel through the following stops on the #{line_start} line: #{new_trip}"
-    p "#{total_stop} stops in total."
+    puts "You must travel through the following stops on the #{line_start} line: #{new_trip}"
+    puts "#{total_stop} stops in total."
 
    elsif line_start != line_stop
      # check start of the line to union Square
     first_trip = @train_line[line_start][start_stop..union_line_start]
+    first_trip_print = @train_line[line_start][start_stop..union_line_start].join ', '
     first_trip_length = first_trip.length
      # check union square to end stops
       if union_line_stop < end_stop
         second_trip = @train_line[line_stop][union_line_stop..end_stop]
-      # elsif union_line_stop > end_stop
-      #   reverse_second_line = line_stop.reverse
-      #   second_trip = @train_line[line_stop][end_stop..union_line_stop]
+        second_trip_print = @train_line[line_stop][union_line_stop..end_stop].join ', '
+
+      elsif union_line_stop > end_stop
+        # reverse_second_line = line_stop.reverse
+        second_trip = @train_line[line_stop][end_stop..union_line_stop].reverse
+        second_trip_print = @train_line[line_stop][end_stop..union_line_stop].reverse.join ', '
       end
-     #find first line lenght
-
-     # find second line length
-
-     # add togther - 1
 
      total_stop = first_trip.length + second_trip.length - 1
-     p "You must travel through the following stops on the #{line_start} line: #{first_trip}"
-     p "Chnage at Union Square"
-     p "Your journey continues through the following stops: #{second_trip}"
-     p "#{total_stop} stops in total."
-
-
-
-
-    # train_line["N"].index "Union Square"
-
+     puts "You must travel through the following stops on the #{line_start} line: #{first_trip_print}"
+     puts "Chnage at Union Square"
+     puts "Your journey continues through the following stops: #{second_trip_print}"
+     puts "#{total_stop} stops in total."
 
   end
 
 end
-
-plan_trip 'N', '34th', 'L', "3rd"
-
-
-
-#plan_trip 'N', 'Times Square', '6', '33rd'
-# `puts` shows output similar to this:
-# # "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
-# # "Change at Union Square."
-# # "Your journey continues through the following stops: 23rd, 28th, 33rd."
-# # "7 stops in total."
+print plan_trip line_start, start, line_stop, stop

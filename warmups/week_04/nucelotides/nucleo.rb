da58f6fe0127c83f1e35facada0ddc3933772fed
John Coote <john_coote@rocketmail.com>
2018-07-04 10:26:17 +1000

    comment_version

--- warmups/week_04/nucelotides/nucleo.rb
+++ warmups/week_04/nucelotides/nucleo.rb
@@ -73,3 +73,84 @@ def find_tides_with_count word
 end
 
 p find_tides_with_count "CGTETYAYFNFGJJUUJU"
+
+
+
+
+
+# Here is a beautifully commented version from GA guru Josh McEvoy
+#
+# require 'pry'
+#
+# # defining our object with a starting count of zero
+# @nucleotides = {
+#   A: 0,
+#   C: 0,
+#   G: 0,
+#   T: 0
+# }
+#
+# def count_nucleotides word
+#   # upcasing and splitting the string to account for strange user inputs then gain access to the individual characters.
+#   word = word.upcase.split('')
+#
+#   # using an each loop to iterate through each character
+#   word.each do | letter |
+#     # in the first iteration letter => 'A'
+#     @nucleotides.each do | key, value |
+#
+#       # checking if the letter is strictly equal to the keys in the @nucleotides object.
+#       if key.to_s == letter
+#
+#         # adding one onto the value of the @nucleotides
+#         @nucleotides[ key ] += 1
+#
+#       end
+#
+#     end
+#
+#   end
+#
+#   # returning the modified object.
+#   @nucleotides
+#
+# end
+#
+# puts count_nucleotides('ACTGAOFHHNIECT')
+#
+#
+# # -------------------------------------------------------
+# # -------------------------------------------------------
+# # -------------------------------------------------------
+#
+#
+# # Alternative to the instance variable - Use a method.
+# # The implicit return immediately spits out the hash you create -- You can read this within methods.
+#
+# def nucleotide_score
+#   {
+#     A: 0,
+#     C: 0,
+#     G: 0,
+#     T: 0
+#   }
+# end
+#
+# def count_nucleotides_in word
+#   word = word.upcase.split('')
+#
+#   # Create a blank score using the nucleotide_score method above.
+#   score = nucleotide_score
+#
+#     nucleotide_score.each do | key, value |
+#
+#       # Ruby gives us the .count method. ["A, B, A, C, A"].count( "A" ) => 3.
+#       # This means we don't actually need to iterate our word array, we can just add our count to the score key's value.
+#       score[ key ] = word.count( key.to_s )
+#     end
+#
+#   score
+#
+# end
+#
+# p count_nucleotides_in 'ACTGAOFHHNIECT'

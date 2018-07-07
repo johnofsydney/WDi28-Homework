1653431d92e80f3143daef638d3a112be58c7fbd
sonicakes <soniaparker1207@gmail.com>
2018-07-07 14:57:17 +1000

    Changed DB data

--- sonia_parker/week_04/day_05/eminem/eminem.sql
+++ sonia_parker/week_04/day_05/eminem/eminem.sql
@@ -16,6 +16,6 @@ INSERT INTO albums VALUES(1, "Revival", 2017, "https://s3.amazonaws.com/hiphopdx
 INSERT INTO albums VALUES(2, "Recovery", 2010, "https://upload.wikimedia.org/wikipedia/en/6/60/Recovery_Album_Cover.jpg");
 INSERT INTO albums VALUES(3, "Relapse", 2009, "https://upload.wikimedia.org/wikipedia/en/4/42/Relapse_%28album%29.jpg");
 
-INSERT INTO songs VALUES(1, "River", "https://www.youtube.com/watch?v=wfWIs2gFTAM", 1);
-INSERT INTO songs VALUES(2, "Not Afraid", "https://www.youtube.com/watch?v=j5-yKhDd64s", 2);
-INSERT INTO songs VALUES(3, "Walk on Water", "https://www.youtube.com/watch?v=ryr75N0nki0", 1);
+INSERT INTO songs VALUES(1, "River", "wfWIs2gFTAM", 1);
+INSERT INTO songs VALUES(2, "Not Afraid", "j5-yKhDd64s", 2);
+INSERT INTO songs VALUES(3, "Walk on Water", "ryr75N0nki0", 1);

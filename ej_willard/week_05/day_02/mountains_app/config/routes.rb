894ddb27f45050d91c932693de1557e267e75b4f
ellijayne <ellijayne.m.willard@gmail.com>
2018-07-11 09:38:03 +1000

    adding mountains

--- ej_willard/week_05/day_02/mountains_app/config/routes.rb
+++ ej_willard/week_05/day_02/mountains_app/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'mountains#index'
+  get '/mountains' => 'mountains#index'
+
+  get '/mountains/new' => 'mountains#new'
+  post '/mountains' => 'mountains#create'
+
+  get '/mountains/:id' => 'mountains#show', :as => :mountain
+
+  get '/mountains/:id/edit' => 'mountains#edit', :as => :mountain_edit
+  post '/mountains/:id' => 'mountains#update', :as => :mountain_update
+
+
+  delete '/mountains/:id' => 'mountains#destroy'
 end

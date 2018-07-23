f0528fe190d218f6050d384e16a2f8d6376c393f
Soheil <Soheil.Tajahmadi@gmail.com>
2018-07-23 16:42:28 +1000

    working draft, needs styling

---
+++ soheil_tajahmadi/week_07/day_01/js/main.js
@@ -0,0 +1,40 @@
+console.log('connected');
+
+const grabBook = function() {
+  let query = document.getElementById('query').value;
+  const xhr = new XMLHttpRequest;
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4 ) {
+      return
+    }
+    let  searchResult = JSON.parse (xhr.responseText)["items"]
+    console.log(searchResult);
+
+    let resultTag = document.createElement('div');
+    for (var i = 0; i < searchResult.length; i++) {
+      let title = searchResult[i]["volumeInfo"]["title"];
+      let image = searchResult[i]["volumeInfo"]["imageLinks"]["thumbnail"];
+
+      let titleTag = document.createElement('p');
+      let imageTag = document.createElement('img');
+      // debugger;
+      titleTag.innerHTML =  title;
+      imageTag.src = image;
+      let box = document.createElement('div');
+      box.appendChild(imageTag);
+      box.appendChild(titleTag);
+      resultTag.appendChild(box);
+      // debugger;
+    }
+    document.getElementById('results').appendChild(resultTag);
+  }
+
+  let httpQuery = 'https://www.googleapis.com/books/v1/volumes?q=title:' + query
+  // debugger;
+  xhr.open('GET',httpQuery)
+  xhr.send();
+
+  // debugger;
+}
+
+document.getElementById('getbook').addEventListener('click', grabBook)

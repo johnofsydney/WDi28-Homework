35a3aab664b3b3e004f52421caac0a5d2d647881
williamlieu <william.lieu@outlook.fr>
2018-07-24 09:18:40 +1000

    goodbooks

--- william_lieu/week_05/day_04/Foods/Cooking/db/migrate/20180712072659_create_foods.rb
+++
@@ -1,9 +0,0 @@
-class CreateFoods < ActiveRecord::Migration[5.2]
-  def change
-    create_table :foods do |t|
-    t.text :name
-    t.integer : :recipe_id
-    t.integer : :cuisine_id
-    end
-  end
-end

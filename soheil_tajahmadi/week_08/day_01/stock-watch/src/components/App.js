fa9a6a07e3a95418d48e63f8b4a30aa60a3f30d9
Soheil <Soheil.Tajahmadi@gmail.com>
2018-07-30 23:56:42 +1000

    week 08, day 01 homework, react stockQuote

---
+++ soheil_tajahmadi/week_08/day_01/stock-watch/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Stockwatch from './Stockwatch'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Stockwatch />
+      </div>
+    );
+  }
+}
+
+
+export default App;

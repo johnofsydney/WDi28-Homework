c9b51735b69f9d3184de963430b03405f1ea5d3a
John Coote <john_coote@rocketmail.com>
2018-08-15 08:30:57 +1000

    wordy-pre

---
+++ warmups/week_10/wordy_calculator/word_problem.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+
+# I've been quite nice and provided the skeleton of the solution. All you have to do is write the code.
+# You can use regex.
+
+class WordProblem
+  # The test will generate a new instance of this class
+
+  def initialize question
+    #  You'll probably need this...
+  end
+
+  #  You'll maybe need some internal methods. Up to you...
+
+  def answer
+    # You certasinly need this - it's referred to in the test file.
+  end
+
+end
